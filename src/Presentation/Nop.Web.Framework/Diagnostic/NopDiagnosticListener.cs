e874442b56c1fc140189f1370aaf8b8edce4fd1f
RomanovM <maxim.r@nopcommerce.com>
2017-10-10 16:04:22 +0300

    Issue 2607 Add ViewRendering event

--- src/Presentation/Nop.Web.Framework/Diagnostic/NopDiagnosticListener.cs
+++ src/Presentation/Nop.Web.Framework/Diagnostic/NopDiagnosticListener.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.Extensions.DiagnosticAdapter;
 using Nop.Core;
 using Nop.Core.Data;
 using Nop.Core.Infrastructure;
+using Nop.Services.Events;
 using Nop.Services.Logging;
+using Nop.Web.Framework.Events;
 
 namespace Nop.Web.Framework.Diagnostic
 {
@@ -35,6 +39,20 @@ namespace Nop.Web.Framework.Diagnostic
             //log error
             var error = $"The view '{viewName}' was not found. The following locations were searched:{Environment.NewLine}{string.Join(Environment.NewLine, searchedLocations)}";
             logger.Error(error, customer: workContext.CurrentCustomer);
+        }
+
+        /// <summary>
+        /// Publish events before view is rendered
+        /// </summary>
+        /// <param name="view">View</param>
+        /// <param name="viewContext">View context</param>
+        [DiagnosticName("Microsoft.AspNetCore.Mvc.BeforeView")]
+        public virtual void BeforeView(IView view, ViewContext viewContext)
+        {
+            var eventPublisher = EngineContext.Current.Resolve<IEventPublisher>();
+            
+            //publish event
+            eventPublisher.Publish(new ViewRenderingEvent(view, viewContext));
         }
     }
 }
---
+++ src/Presentation/Nop.Web.Framework/Events/ViewRenderingEvent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+
+namespace Nop.Web.Framework.Events
+{
+    /// <summary>
+    /// Represents an event that occurs before the view is rendered
+    /// </summary>
+    public class ViewRenderingEvent
+    {
+        #region Ctor
+
+        public ViewRenderingEvent(IView view, ViewContext viewContext)
+        {
+            this.View = view;
+            this.ViewContext = viewContext;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets a view
+        /// </summary>
+        public IView View { get; private set; }
+
+        /// <summary>
+        /// Gets a view context
+        /// </summary>
+        public ViewContext ViewContext { get; private set; }
+
+        #endregion
+    }
+}
